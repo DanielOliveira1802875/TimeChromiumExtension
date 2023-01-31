9b3068b523669a241aeb3769cd8f03db38380eff
Daniel_Oliveira <oliveiradaniel@outlook.com>
2023-01-31 06:39:50 +0000

    First commit of the time extension for browsers that are based on chromium.

---
+++ README.md
@@ -0,0 +1,7 @@
+# TimeChromiumExtension
+Changes the user interface of "Time" in browsers based on chromium.
+
+## Installation
+- Use "npm run build" to build the extension, the extension will be in the folder ./dist.
+- Go to chrome://extensions in Chrome and turn on developer mode.
+- Then click "load unpacked" and pick the folder you built.
---
+++ src/index.ts
@@ -0,0 +1,93 @@
+import {format, differenceInMinutes, parse, isDate} from "date-fns";
+import {isNumber} from "lodash";
+
+const calcWorkingMinutes = (hoursArray: Date[]) => {
+    if (hoursArray.length <= 1) return 0;
+    if (hoursArray.length < 4) {
+        const [first, second] = hoursArray;
+        return differenceInMinutes(second, first);
+    }
+    if (hoursArray.length === 4) {
+        const [first, second, third, fourth] = hoursArray;
+        return differenceInMinutes(second, first) + differenceInMinutes(fourth, third);
+    }
+    return 0;
+}
+
+
+const appendSpan = (element: Element, minutes: number) => {
+    if(isNaN(minutes)) return;
+    const span = document.createElement('span');
+    const shouldWorkMinutes = 8*60;
+
+    const diff = minutes - shouldWorkMinutes;
+
+    if (diff < -120 ) span.style.color = 'red';
+    else if (diff < 0) span.style.color = 'darkorange';
+    else span.style.color = 'green';
+    span.style.fontWeight = 'bold';
+    span.style.whiteSpace = 'pre-wrap'
+    const date = new Date((Math.abs(diff)-60) * 60 * 1000);
+    const formatted = format(date, 'HH:mm');
+    span.innerText = `   ${diff < 0 ? '-': '+'} ${formatted} ⌛   `;
+    element.appendChild(span);
+}
+
+const worker = () => {
+    const currentDay = document.getElementById('ctl00_ASPxSplitter_cphContent_painelMovimentosAssiduidade_WMovimentosAssiduidade_cpMovimentosAssiduidade_txtDiaCorrente')
+    if (currentDay !== null) {
+        let currentDayHoursStr = currentDay?.innerText?.split(' - ')
+        if (currentDayHoursStr.length  === 0) return;
+        if (currentDayHoursStr.length < 4) {
+            currentDayHoursStr.push(format(new Date(), 'HH:mm'))
+        }
+        let currentDayHours = currentDayHoursStr.map((hour) => {
+            const date = parse(hour, 'HH:mm', new Date())
+            if (isDate(date)) return date;
+            return null;
+        }).filter((date) => date !== null) as Date[];
+        const workingHours = calcWorkingMinutes(currentDayHours);
+        appendSpan(currentDay, workingHours);
+    }
+
+
+    const previousDay = document.getElementById('ctl00_ASPxSplitter_cphContent_painelMovimentosAssiduidade_WMovimentosAssiduidade_cpMovimentosAssiduidade_txtDiaAnterior')
+    if (previousDay !== null) {
+        const lastDayHours = previousDay?.innerText?.split(' - ')
+        if (lastDayHours.length === 0) return;
+        const lastDayHoursParsed = lastDayHours.map((hour) => {
+            const date = parse(hour, 'HH:mm', new Date())
+            if (isDate(date)) return date;
+            return null
+        }).filter((date) => date !== null) as Date[];
+
+        const workingHours = calcWorkingMinutes(lastDayHoursParsed);
+        appendSpan(previousDay, workingHours);
+    }
+
+    const rows = document.querySelectorAll('[id*=ctl00_ASPxSplitter_cphContent_grid_DXDataRow]');
+    if (rows.length > 0) {
+        rows.forEach(row => {
+            const columns = row?.children;
+            let hours: string[] = []
+            hours.push(columns[6].innerHTML)
+            hours.push(columns[7].innerHTML)
+            hours.push(columns[8].innerHTML)
+            hours.push(columns[9].innerHTML)
+            const dates = hours.reduce((acc: Date[], curr) => {
+                if (/\d\d:\d\d/.test(curr.toString())) acc.push( parse(curr.toString(), 'HH:mm', new Date()) )
+                return acc
+            }, [])
+            if (hours.length === 0) return;
+            const workingMinutes = calcWorkingMinutes(dates);
+            if (workingMinutes === 0) return;
+            columns[11].innerHTML = '';
+            appendSpan(columns[11], workingMinutes)
+        })
+    }
+    setTimeout(worker, 1000)
+}
+
+worker()
+
+export default worker;
